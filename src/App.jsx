c8c4695f1bb41487bb4cc0c2a9c1a24d6f651bbb
DmitriyALEX <doiroffical@gmail.com>
2025-07-30 21:24:58 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import './App.scss';
+import React from 'react'
+import './App.scss'
+import { Person } from './components/Person/Person'
 
 export const misha = {
   name: 'Misha',
@@ -7,39 +8,26 @@ export const misha = {
   sex: 'm',
   isMarried: true,
   partnerName: 'Natasha',
-};
+}
 
 export const olya = {
   name: 'Olya',
   sex: 'f',
   isMarried: true,
   partnerName: 'Maksym',
-};
+}
 
 export const alex = {
   name: 'Alex',
   age: 25,
   sex: 'm',
   isMarried: false,
-};
+}
 
 export const App = () => (
   <div className="App">
-    <section className="Person">
-      <h2 className="Person__name">My name is Misha</h2>
-      <p className="Person__age">I am 37</p>
-      <p className="Person__partner">Natasha is my wife</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Olya</h2>
-      <p className="Person__partner">Maksym is my husband</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Alex</h2>
-      <p className="Person__age">I am 25</p>
-      <p className="Person__partner">I am not married</p>
-    </section>
+    <Person person={misha} />
+    <Person person={olya} />
+    <Person person={alex} />
   </div>
-);
+)
